eea3a77081a9bc4620573ad28d52bfce2f742791
darzwaitz <darzwaitz@yahoo.com>
2024-03-05 18:14:30 +0000

    initial filez - this commit can be used for boilerplate setup - vite react tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-green-500">First page</h1>
+      <h2 className="text-orange-300  font-medium ">2nd heading</h2>
+      <h3 className=" text-green-600  font-bold ">Tailwind working</h3>
+    </div>
+  );
+}
+
+export default App;
