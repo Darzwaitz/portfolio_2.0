50db9f7f1e13fee680265ca34cc3019d0e562c72
darzwaitz <darzwaitz@yahoo.com>
2024-05-16 23:58:24 +0100

    adding initial SidebarNav comp & temp styles

---
+++ src/ui/SidebarNav.jsx
@@ -0,0 +1,9 @@
+function SidebarNav() {
+  return (
+    <aside className=" bg-red-300 fixed top-0 left-0 gap-x-40 w-64 h-screen z-auto">
+      sidebar
+    </aside>
+  );
+}
+
+export default SidebarNav;
