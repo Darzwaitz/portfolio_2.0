3bb00e2eeed68854cb21bd969a1ec6d4b0ce7e68
darzwaitz <darzwaitz@yahoo.com>
2024-05-31 23:57:35 +0100

    setting up terminal comp in AppLayout - Part 2

--- src/features/terminal/Terminal.jsx
+++ src/features/terminal/Terminal.jsx
@@ -1,5 +1,9 @@
 function Terminal() {
-    return <div>Terminal temp.</div>
+    return (
+        <div role="terminal" className="absolute bottom-0 h-6 w-auto">
+            <h1>Terminal temp.</h1>
+        </div>
+    )
 }
 
 export default Terminal
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -11,13 +11,13 @@ function AppLayout() {
             <main className="bg-yellow-400 flex h-[calc(100vh-72px)] w-full">
                 <SidebarNav />
 
-                <section className="w-full bg-black-01 text-grey-01">
+                <section className="relative w-full bg-black-01 text-grey-01">
                     <Outlet />
+                    <Terminal />
                 </section>
                 {/* tmp for testing */}
                 {/* <SidebarNav /> */}
             </main>
-            <Terminal />
             <Footer />
         </>
     )
