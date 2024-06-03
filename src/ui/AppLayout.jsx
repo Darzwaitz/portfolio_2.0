e5158701eed3902139b6a515a608b6deb7f7f488
darzwaitz <darzwaitz@yahoo.com>
2024-06-03 23:54:49 +0100

    setting up panels state in AppLayout - Part 1

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,17 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 import Header from './header/Header'
 import SidebarNav from './SidebarNav'
 import Terminal from '../features/terminal/Terminal'
 import Footer from './Footer'
 
 function AppLayout() {
+    const [togglePanels, setTogglePanels] = useState({
+        toggleLeft: true,
+        toggleTerminal: false,
+        togglePanelOptions: false,
+        toggleRight: false,
+    })
+    function handleTogglePanels(panel) {
+        setTogglePanels((prevPanel) => ({ ...togglePanels }))
+    }
     return (
         <>
             <Header />
             <main className="bg-yellow-400 flex h-[calc(100vh-72px)] w-full">
                 <SidebarNav />
 
-                <section className="relative w-full bg-black-01 text-grey-01">
+                <section className="relative -z-10 w-full bg-black-01 text-grey-01">
                     <Outlet />
                     <Terminal />
                 </section>
