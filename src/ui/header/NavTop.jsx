9d76d4f9fba55306b4b0a2ce1c4934f4005fddc4
darzwaitz <darzwaitz@yahoo.com>
2024-06-10 23:31:03 +0100

    fixed main nav styles

--- src/ui/header/NavTop.jsx
+++ src/ui/header/NavTop.jsx
@@ -2,13 +2,6 @@ import { NavLink } from 'react-router-dom'
 
 function NavTop() {
     return (
-        // <nav className="text-grey-01 flex gap-5">
-        //     <Link to="/">Home</Link>
-        //     <Link to="/about">About</Link>
-        //     <Link to="/portfolio">Portfolio</Link>
-        //     <Link to="/contact">Contact</Link>
-        // </nav>
-
         <nav className="flex gap-5 text-grey-01">
             {[
                 ['Home', '/'],
@@ -17,12 +10,12 @@ function NavTop() {
                 ['Contact', '/contact'],
             ].map(([title, url]) => (
                 <NavLink
-                    key={Math.random()}
-                    // className="  active:text-grey-08"
+                    key={title}
                     className={({ isActive }) =>
-                        isActive ? 'text-grey-02' : ''
+                        !isActive
+                            ? ' pb-1 pl-1.5 pr-2 hover:text-grey-02'
+                            : 'rounded-md bg-grey-04 pb-1 pl-1.5 pr-2 text-grey-02'
                     }
-                    // className="text-slate-700 hover:bg-slate-100 hover:text-slate-900 rounded-lg px-3 py-2 font-medium"
                     to={url}
                 >
                     {title}
