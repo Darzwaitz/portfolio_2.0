a4d4ebda94aa16238c6a18ca8ff63e610c9d6aff
darzwaitz <darzwaitz@yahoo.com>
2024-06-12 23:59:20 +0100

    setting up explorer bar - sub sidebar - adding some styles - Part 2

--- src/ui/sidebar/SidebarExplorer.jsx
+++ src/ui/sidebar/SidebarExplorer.jsx
@@ -1,10 +1,20 @@
 export default function SidebarExplorer() {
     return (
         <ul className="border-l-[1px] border-r-[1px] border-l-grey-04 border-r-grey-04 bg-black-01  text-grey-01">
-            <li>EXPLORER</li>
+            {/* <li>EXPLORER</li>
             <li>ALL PROJECTS</li>
             <li>SEARCH PROJECTS</li>
-            <li>SHOWCASE PROJECTS</li>
+            <li>SHOWCASE PROJECTS</li> */}
+            {[
+                'EXPLORER',
+                'ALL PROJECTS',
+                'SEARCH PROJECTS',
+                'SHOWCASE PROJECTS',
+            ].map((title) => (
+                <li key={title} className="text-grey-02">
+                    {title}
+                </li>
+            ))}
         </ul>
     )
 }
