63b7eddba1e0895b5e4934a98baf72711bb70613
darzwaitz <darzwaitz@yahoo.com>
2024-06-13 23:52:42 +0100

    adding a new arrow icon comp. - adding styles - Part 1

--- src/ui/sidebar/SidebarExplorer.jsx
+++ src/ui/sidebar/SidebarExplorer.jsx
@@ -1,7 +1,9 @@
-export default function SidebarExplorer() {
+import ArrowIcon from './assets/imgs/svg/components/ArrowIcon'
+
+function SidebarExplorer() {
     return (
-        <section className="border-l-[1px] border-r-[1px] border-l-grey-04 border-r-grey-04  bg-black-01 text-grey-01">
-            <ul className=" mt-4 ">
+        <section className="w-max border-l-[1px] border-r-[1px] border-l-grey-04  border-r-grey-04 bg-black-01 text-grey-01">
+            <ul className=" mt-4 flex flex-col">
                 {/* <li>EXPLORER</li>
             <li>ALL PROJECTS</li>
             <li>SEARCH PROJECTS</li>
@@ -14,12 +16,17 @@ export default function SidebarExplorer() {
                 ].map((title) => (
                     <li
                         key={title}
-                        className=" text-sm text-grey-02 hover:brightness-110"
+                        className="flex text-sm text-grey-02 hover:brightness-110"
                     >
-                        <h1 className="text-nowrap ">{title}</h1>
+                        <span className="">
+                            <ArrowIcon />
+                        </span>
+                        <h1 className="">{title}</h1>
                     </li>
                 ))}
             </ul>
         </section>
     )
 }
+
+export default SidebarExplorer
