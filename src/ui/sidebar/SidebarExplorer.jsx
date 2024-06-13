d651108d4525f95b90393a8f3e001a994d6ce720
darzwaitz <darzwaitz@yahoo.com>
2024-06-13 23:38:37 +0100

    adding a new arrow icon comp.

--- src/ui/sidebar/SidebarExplorer.jsx
+++ src/ui/sidebar/SidebarExplorer.jsx
@@ -1,20 +1,25 @@
 export default function SidebarExplorer() {
     return (
-        <ul className="border-l-[1px] border-r-[1px] border-l-grey-04 border-r-grey-04 bg-black-01  text-grey-01">
-            {/* <li>EXPLORER</li>
+        <section className="border-l-[1px] border-r-[1px] border-l-grey-04 border-r-grey-04  bg-black-01 text-grey-01">
+            <ul className=" mt-4 ">
+                {/* <li>EXPLORER</li>
             <li>ALL PROJECTS</li>
             <li>SEARCH PROJECTS</li>
             <li>SHOWCASE PROJECTS</li> */}
-            {[
-                'EXPLORER',
-                'ALL PROJECTS',
-                'SEARCH PROJECTS',
-                'SHOWCASE PROJECTS',
-            ].map((title) => (
-                <li key={title} className="text-grey-02">
-                    {title}
-                </li>
-            ))}
-        </ul>
+                {[
+                    'EXPLORER',
+                    'ALL PROJECTS',
+                    'SEARCH PROJECTS',
+                    'SHOWCASE PROJECTS',
+                ].map((title) => (
+                    <li
+                        key={title}
+                        className=" text-sm text-grey-02 hover:brightness-110"
+                    >
+                        <h1 className="text-nowrap ">{title}</h1>
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 }
