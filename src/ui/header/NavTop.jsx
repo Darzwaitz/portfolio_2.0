6d08e0d735956749e26f9cab282b17c2fbeccbef
darzwaitz <darzwaitz@yahoo.com>
2025-04-14 23:19:43 +0100

    some comment cleanup and renamed extensionz for 2 filez

--- src/ui/header/NavTop.jsx
+++ src/ui/header/NavTop.jsx
@@ -23,19 +23,3 @@ function NavTop() {
 }
 
 export default NavTop
-
-// function NavTop() {
-//     const { curPageList } = useCurPage()
-
-//     console.log(curPageList)
-//     const displayNavArr = curPageList.shift()
-
-//     return (
-//         <nav className="text-grey-01 flex gap-5">
-//             {/* {curPageList.map(([title, icon, url]) => ( */}
-//             {displayNavArr.map(([title, icon, url]) => (
-//                 <NavItems key={title} title={title} url={url} icon={icon} />
-//             ))}
-//         </nav>
-//     )
-// }
