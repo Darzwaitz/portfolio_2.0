77d94577e9be728c82a8b01ad5569950307786d2
darzwaitz <darzwaitz@yahoo.com>
2025-05-07 23:33:32 +0100

    small refactor update on portfolio list items

--- src/features/portfolio/PortfolioFilteritems.jsx
+++ src/features/portfolio/PortfolioFilteritems.jsx
@@ -3,20 +3,20 @@ import { useFilter } from '@/contexts/FilteritemsContext'
 
 function Checkbox({ isChecked, label, checkHandler, icon }) {
     return (
-        <li className="mr-5">
-            <div className="flex items-center">
-                <input
-                    className="accent-grey-04 mr-1.5"
-                    type="checkbox"
-                    id={label}
-                    checked={isChecked}
-                    onChange={checkHandler}
-                />
-                <label className="mr-1" htmlFor={label}>
-                    {label}
-                </label>
-                <span className="h-6 w-6">{icon}</span>
-            </div>
+        <li className="mr-5 flex items-center">
+            {/* <div className="flex items-center"> */}
+            <input
+                className="accent-grey-04 mr-1.5"
+                type="checkbox"
+                id={label}
+                checked={isChecked}
+                onChange={checkHandler}
+            />
+            <label className="mr-1" htmlFor={label}>
+                {label}
+            </label>
+            <span className="h-6 w-6">{icon}</span>
+            {/* </div> */}
         </li>
     )
 }
@@ -28,7 +28,7 @@ function PortfolioFilteritems({ flexcolumn }) {
     return (
         <ul
             // className={`mb-5 flex justify-center ${flexcolumn ? 'flex-col' : 'flex-row'}`}
-            className={`mb-5 flex flex-wrap justify-center ${flexcolumn ? 'flex-col' : 'flex-row'}`}
+            className={`m-auto mb-5 flex flex-wrap justify-center ${flexcolumn ? 'flex-col' : 'flex-row'}`}
         >
             {items.map((menuitem, index) => (
                 <React.Fragment key={index}>
