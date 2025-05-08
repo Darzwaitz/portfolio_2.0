f60933c15bd5abbde005cb754b8fae9ead933670
darzwaitz <darzwaitz@yahoo.com>
2025-05-08 23:53:04 +0100

    small refactor update on portfolio list items - Part 2

--- src/features/portfolio/PortfolioFilteritems.jsx
+++ src/features/portfolio/PortfolioFilteritems.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import { useFilter } from '@/contexts/FilteritemsContext'
 
-function Checkbox({ isChecked, label, checkHandler, icon }) {
-    return (
-        <li className="mr-5 flex items-center">
-            {/* <div className="flex items-center"> */}
-            <input
-                className="accent-grey-04 mr-1.5"
-                type="checkbox"
-                id={label}
-                checked={isChecked}
-                onChange={checkHandler}
-            />
-            <label className="mr-1" htmlFor={label}>
-                {label}
-            </label>
-            <span className="h-6 w-6">{icon}</span>
-            {/* </div> */}
-        </li>
-    )
-}
-
+// list
 function PortfolioFilteritems({ flexcolumn }) {
     // from Context
     const { items, onChangeHandle } = useFilter()
@@ -43,6 +24,27 @@ function PortfolioFilteritems({ flexcolumn }) {
                 </React.Fragment>
             ))}
         </ul>
+    )
+}
+
+// list item
+function Checkbox({ isChecked, label, checkHandler, icon }) {
+    return (
+        <li className="mr-5 flex items-center">
+            {/* <div className="flex items-center"> */}
+            <input
+                className="accent-grey-04 mr-1.5"
+                type="checkbox"
+                id={label}
+                checked={isChecked}
+                onChange={checkHandler}
+            />
+            <label className="mr-1" htmlFor={label}>
+                {label}
+            </label>
+            <span className="h-6 w-6">{icon}</span>
+            {/* </div> */}
+        </li>
     )
 }
 
