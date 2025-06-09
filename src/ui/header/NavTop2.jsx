4193cdf7c6179ec4c0799da74bd59694bda2419e
darzwaitz <darzwaitz@yahoo.com>
2025-06-09 23:59:40 +0100

    working on conditionally showing main navigation - navtop comp - on smaller screens - Part 3 - TBU

--- src/ui/header/NavTop2.jsx
+++ src/ui/header/NavTop2.jsx
@@ -10,7 +10,8 @@ function NavTop() {
 
     return (
         // <nav className="text-grey-01 hidden gap-5 lg:flex">
-        <nav className="text-grey-01 gap-5 lg:flex">
+        // <nav className="text-grey-01 gap-5 lg:flex">
+        <nav className="text-grey-01 absolute gap-5">
             {updatedNavLinks.map(([title, icon, url]) => (
                 <NavItems key={title} title={title} url={url} icon={icon} />
             ))}
