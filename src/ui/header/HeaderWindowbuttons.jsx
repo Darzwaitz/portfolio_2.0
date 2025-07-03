8c6b7d3468dba4c1b8330e545edadcd7fb31a56d
darzwaitz <darzwaitz@yahoo.com>
2025-07-03 23:43:18 +0100

    stylez updatez in header comp - Part 1

--- src/ui/header/HeaderWindowbuttons.jsx
+++ src/ui/header/HeaderWindowbuttons.jsx
@@ -1,6 +1,6 @@
 function HeaderWindowbuttons() {
     return (
-        <div className="hidden gap-2 lg:flex">
+        <div className="hidden gap-2 pl-1 lg:flex">
             <div className="text-grey-01 p-1">-</div>
             <div className="text-grey-01 p-1">◻</div>
             <div className="text-grey-01 p-1">X</div>
