8cd07b9bfc6e26f5f4537a942540562fbeeb7b99
darzwaitz <darzwaitz@yahoo.com>
2025-09-29 23:58:54 +0100

    adding remote button and icon in footer - Part 1

--- src/ui/footer/Footer.jsx
+++ src/ui/footer/Footer.jsx
@@ -1,9 +1,11 @@
+import RemoteButton from './RemoteButton'
+
 function Footer() {
     return (
-        <footer className="border-t-grey-04 bg-black-01 text-grey-01 w-full border-t">
+        <footer className="border-t-grey-04 bg-black-01 text-grey-01 h-fit w-full border-t">
             {/* <div>Footer section</div> */}
-            <span>Footer section</span>
-            <span>Footer section</span>
+            <RemoteButton />
+            {/* <span>Footer section</span> */}
             {/* <div>Footer section</div> */}
         </footer>
     )
---
+++ src/ui/footer/RemoteButton.jsx
@@ -0,0 +1,11 @@
+import RemoteIcon from './assets/imgs/svg/components/RemoteIcon'
+
+function RemoteButton() {
+    return (
+        <span className="h-4 w-4">
+            <RemoteIcon />
+        </span>
+    )
+}
+
+export default RemoteButton
