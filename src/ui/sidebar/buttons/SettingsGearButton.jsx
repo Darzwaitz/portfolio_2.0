dc1749cb0ba16abba45fbba3c7a982cc17fd3f54
darzwaitz <darzwaitz@yahoo.com>
2025-11-03 23:57:10 +0000

    light mode toggle stored in localstorage - Part 1 - TBU

--- src/ui/sidebar/buttons/SettingsGearButton.jsx
+++ src/ui/sidebar/buttons/SettingsGearButton.jsx
@@ -7,18 +7,41 @@ import SettingsGearIcon from '../assets/imgs/svg/components/SettingsGearIcon'
 import Tooltip from '@/features/tooltip/Tooltip'
 
 import { usePanels } from '@/contexts/PanelsContext'
+import { useState } from 'react'
 
-const toggleLightMode = () => document.documentElement.classList.toggle('light')
+// let theme = localStorage.getItem('theme')
+console.log(localStorage.getItem('theme'))
 
 function SettingsGearButton() {
     const { togglePanels } = usePanels()
     let arrDir = togglePanels.tooltipArrDir
 
+    // let [darkMode, setDarkMode] = useState(true)
+    let [darkMode, setDarkMode] = useState(true)
+
+    const toggleLightMode = () => {
+        document.documentElement.classList.toggle('light')
+
+        localStorage.setItem('theme', false)
+        // if (localStorage.getItem('light')) {
+        //     localStorage.setItem('colorMode', 'dark')
+        // } else {
+        //     localStorage.setItem('colorMode', 'light')
+        // }
+
+        console.log(localStorage.getItem('theme'))
+
+        setDarkMode(!darkMode)
+    }
+
     return (
         <SvgIconwrapper iconSize={iconSize} hover={hover}>
             <div onClick={toggleLightMode}>
                 <SettingsGearIcon hover={hover} defaultColor={defaultColor} />
-                <Tooltip msg={'Colour Scheme'} arrowDirection={arrDir} />
+                <Tooltip
+                    msg={darkMode ? 'Light Mode' : 'Dark Mode'}
+                    arrowDirection={arrDir}
+                />
             </div>
         </SvgIconwrapper>
     )
