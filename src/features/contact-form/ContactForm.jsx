a9d55f97d141aa453754781dd7739687dc0d9517
darzwaitz <darzwaitz@yahoo.com>
2025-11-15 23:42:32 +0000

    Updating contact form stylez - Part 1b

--- src/features/contact-form/ContactForm.jsx
+++ src/features/contact-form/ContactForm.jsx
@@ -1,26 +1,30 @@
 function ContactForm() {
     return (
         <div>
-            <form className="mx-auto my-12 max-w-lg">
+            <form className="mx-auto my-12 flex max-w-lg flex-col gap-4">
                 <input
                     name="name"
                     type="text"
-                    className="feedback-input"
+                    className="input-box-style-one"
                     placeholder="Name"
                 />
                 <input
                     name="email"
                     type="text"
-                    className="feedback-input"
+                    className="input-box-style-one"
                     placeholder="Email"
                 />
                 <textarea
                     name="text"
                     // className="feedback-input"
-                    className="font-(500) focus:borde mb-3.5 box-border w-full border-2 border-r-4 border-solid border-current p-3 text-base/22 text-current outline-0 transition-[0.3s] focus:border-2"
+                    className="font-(500) mb-3.5 box-border w-full border border-solid border-current p-3 text-base/22 text-current outline-0 transition-[0.3s] focus:border-2"
                     placeholder="Comment"
                 ></textarea>
-                <input type="submit" value="SUBMIT" />
+                <input
+                    className="hover:bg-grey-01 hover:text-grey-02 tr font-(700) -mt-1 w-full cursor-pointer border-0 py-2.5 text-2xl transition-[0.3s]"
+                    type="submit"
+                    value="SUBMIT"
+                />
             </form>
             {/* <form>
                 <div>
